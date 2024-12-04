abc10930847fcd072ee8de45cc7dea8f19a85d15
Inkurey22 <inkurey22.tr@proton.me>
2024-12-04 09:05:05 +0100

    feat: added default value to kill command

--- src/kill.c
+++ src/kill.c
@@ -14,20 +14,20 @@ void manage_signal(const int pid)
     int sig = 0;
     char sig_str[10];
 
-    mvprintw(5, 0, "Enter signal to send to %d : ", pid);
+    mvprintw(5, 0, "Enter signal to send to %d [DEFAULT : SIGTERM]: ", pid);
     echo();
     getnstr(sig_str, 10);
     noecho();
+    mvprintw(5, 0, "%*s", COLS, "");
     sig = atoi(sig_str);
-    if (sig > 0) {
-        if (kill(pid, sig) == 0)
-            mvprintw(5, 0, "Signal %d sent to process %d successfully.",
-                sig, pid);
-        else
-            mvprintw(5, 0, "Failed to send signal %d to process %d.",
-                sig, pid);
-    } else
-        mvprintw(5, 0, "Invalid signal. Press any key to continue.");
+    if (sig <= 0)
+        sig = SIGTERM;
+    if (kill(pid, sig) == 0)
+        mvprintw(5, 0, "Signal %d sent to process %d successfully.",
+            sig, pid);
+    else
+        mvprintw(5, 0, "Failed to send signal %d to process %d.",
+            sig, pid);
 }
 
 void launch_kill(void)
@@ -35,15 +35,16 @@ void launch_kill(void)
     int pid = 0;
     char pid_str[10];
 
-    mvprintw(5, 0, "Enter PID to kill : ");
+    mvprintw(5, 0, "Enter PID to kill [DEFAULT: 1]: ");
     echo();
     getnstr(pid_str, 10);
     noecho();
+    mvprintw(5, 0, "%*s", COLS, "");
     pid = atoi(pid_str);
     if (pid > 0)
         manage_signal(pid);
     else
-        mvprintw(5, 0, "Invalid PID. Press any key to continue.");
+        manage_signal(1);
     getch();
     clear();
 }
